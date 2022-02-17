17039e0e034bda9c07c59a87c996d9a94d5f9d07
todos <pablo_matias_g@hotmail.com>
2022-02-17 19:29:57 -0300

    uno

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -11,5 +11,5 @@ export const HEROES: Hero[] = [
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' },
-  { id: 21, name: 'SuperMatias' }
+  { id: 21, name: 'Matias' }
 ];
